a44c6de62a8021f200d957fb18ccfeb5b3715107
Maxim <fixor@yandex.ru>
2025-02-17 19:51:38 +0300

    grpc

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/SensorHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/SensorHandler.java
@@ -6,5 +6,6 @@ import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 public interface SensorHandler {
     SensorEventProto.PayloadCase getMessageType();
 
+
     void handle(SensorEventProto event);
 }
