b0f9736eb61d81ae64c3c71d5490e9c244d0bf33
Maxim <fixor@yandex.ru>
2025-03-08 00:32:07 +0300

    7-spring-cloud-microservices

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafkaConfig/Config.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafkaConfig/Config.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import org.apache.avro.specific.SpecificRecordBase;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.VoidSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -25,7 +26,7 @@ public class Config {
     public KafkaProducer<String, SpecificRecordBase> getProducer() {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, VoidSerializer.class);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, EventAvroSerializer.class);
 
         return new KafkaProducer<>(configProps);
