3c7f08f1b0da3750f5842e5df4c256761c78e4d0
Maxim <fixor@yandex.ru>
2025-03-08 19:45:18 +0300

    7-spring-cloud-microservices

--- telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/DeviceAddedEvent.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/model/hub/DeviceAddedEvent.java
@@ -1,6 +1,7 @@
 package ru.yandex.practicum.model.hub;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,6 +9,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString(callSuper = true)
+@Data
 public class DeviceAddedEvent extends HubEvent {
 	@NotNull
 	private String id;
